678be6590889acc0ac17e3f2ea000b2302dae5f6
AhmedMuhaameed <ahmed.m.abdelrahman0@gmail.com>
2021-09-23 17:19:00 +0200

    Fix Styling

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
 (function ($) {
     var typed = new Typed("span.txt-rotate", {
-        strings: ["I'm a MEARN Stack Web Developer", "I'm a Full Stack Web Developer", "I'm a Software Enginner"],
+        strings: ["I'm a MEARN Stack Web Developer", "I'm a Full Stack Web Developer", "I'm a Software Engineer"],
         typeSpeed: 100,
         backSpeed: 100,
         fadeOut: false,
         smartBackspace: true,
-        loop: true, 
+        loop: true,
     });
 })(jQuery);
 
+$(document).ready(function () {
+    let $btn = $(".list-group-item");
+    $btn.click(function (e) {
+        $(".list-group-item").removeClass("active");
+        e.target.classList.add("active");
+        let selector = $(e.target).attr("data-filter");
+        $(".projects-wrapper").isotope({
+            filter: selector,
+        });
 
-$(document).ready(function(){
-    let $btn=$('.list-group-item');
-    $btn.click(function(e){
-        $('.list-group-item').removeClass('active');
-        e.target.classList.add('active');
-        let selector =$(e.target).attr('data-filter');
-        $('.projects-wrapper').isotope({
-            filter: selector
-        })
         return false;
-    })
+    });
 });
